8b02644eb8798fcd80b7bfdee7a929b29e2d4e72
Francisco Paredes <paredesfrancisco031@gmail.com>
2022-04-12 20:24:58 -0300

    first commit

---
+++ src/app/pages/action-sheet/action-sheet.page.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+@Component({
+  selector: 'app-action-sheet',
+  templateUrl: './action-sheet.page.html',
+  styleUrls: ['./action-sheet.page.scss'],
+})
+export class ActionSheetPage implements OnInit {
+
+  constructor(public actionSheetController: ActionSheetController) { }
+
+  ngOnInit() {
+  }
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Opciones',
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Eliminar',
+        role: 'destructive',
+        icon: 'trash',
+        id: 'delete-button',
+        data: {
+          type: 'delete'
+        },
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Compartir',
+        icon: 'share',
+        data: 10,
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Reproducir',
+        icon: 'caret-forward-circle',
+        data: 'Data value',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Favoritos',
+        icon: 'heart',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancelar',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const { role, data } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role and data', role, data);
+  }
+
+}
+
